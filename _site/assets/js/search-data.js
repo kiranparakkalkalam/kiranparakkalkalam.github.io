ab1654fdae5211c83cf7e0bd8ea570a491ed069f
Kiran P <pkiran.kala@gmail.com>
2020-05-25 16:00:04 +0530

    Push to gh-pages branch

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Merkel Tree",
+        "excerpt":"Merkel trees also known as Hash Trees are used for data verification and synchronisation. Its a tree where each non-leaf node is the collated hash of all its children. Most of the implementation of Merkel tree is binary [i.e two child nodes under the parnet node], but can have as...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/merkel-tree/"
+      }]
